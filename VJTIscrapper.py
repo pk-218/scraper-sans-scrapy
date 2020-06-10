f8acf891f4b827ddb51aaa6d827fe22cae2b9d56
PankajK <pankajk218@users.noreply.github.com>
2020-06-10 14:58:10 +0530

    Python - BeautifulSoup

---
+++ VJTIscrapper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_news = "https://www.vjti.ac.in/"
+
+headers = {
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Referer': 'http://www.wikipedia.org/',
+        'Connection': 'keep-alive',
+    }
+
+r = requests.get(url=url_news, headers=headers)
+
+soup = BeautifulSoup(r.text, "html.parser")
+customs = soup.find_all("div", {"class": "custom"})
+for custom in customs:
+        try:
+                links = custom.find_all("a")
+                for link in links:
+                        Links = [link.get("href")]
+                        print(Links)
+        except Exception:
+                pass
