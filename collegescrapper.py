cd9d6ef9a59c73cedff80b8c54cccce11466fe83
Pankaj Khushalani <53428217+pk-218@users.noreply.github.com>
2020-06-21 01:37:12 +0530

    Update collegescrapper.py

--- collegescrapper.py
+++ collegescrapper.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, current_app
 from flask_apscheduler import APScheduler
 import requests
 from bs4 import BeautifulSoup
@@ -7,60 +7,46 @@ from collections import defaultdict
 
 app = Flask(__name__)
 scheduler = APScheduler()
+with app.app_context():
 
+        @app.route('/', methods=['GET'])
+        def linkScrapper():
 
-@app.route('/', methods=['GET'])
-def linkScrapper():
-
-        d = []
-        """"  # TODO: Change list to dictionary with keys as [1]th index of the Links (on splitting Links at '/')"""
-        # Keys could be DEGREE, DIPLOMA, Exam_Section and so on
-
-        url_news = "https://www.vjti.ac.in"         # link to scrape links from
-
-        # headers for complying with GET request
-#        headers = {
-#                'Accept-Encoding': 'gzip, deflate, sdch',
-#                'Accept-Language': 'en-US,en;q=0.8',
-#                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-#                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#                'Referer': 'http://www.wikipedia.org/',
-#                'Connection': 'keep-alive',
-#        }
-
-        r = requests.get(url=url_news)  #, headers=headers
-        soup = BeautifulSoup(r.text, "html.parser")
-        customs = soup.find_all("div", {"class": "custom"})
-        for custom in customs:
-                try:
-                        links = custom.find_all("a")
-                        for link in links:
-                                if "https" in link.get("href"):
-                                        Links = link.get("href")    # adding URI to source IDs of links to get URL
-                                        # print(Links)
-                                        d.append(Links)
-                                else:
-                                        Links = url_news + link.get("href")
-                                        # print(Links)
-                                        d.append(Links)
-                except Exception:
-                        links = None
-
-        d.pop(-1)  # removing last item for list which is not required
-        json_dict = defaultdict(list)
-        try:
-                for item in d:
-                        title = item.split('/')[4]
+                d = []
+                url_news = "https://www.vjti.ac.in"         # link to scrape links from
+                r = requests.get(url=url_news)  # , headers=headers
+                soup = BeautifulSoup(r.text, "html.parser")
+                customs = soup.find_all("div", {"class": "custom"})
+                for custom in customs:
                         try:
-                                json_dict[title].append(item)
+                                links = custom.find_all("a")
+                                for link in links:
+                                        if "https" in link.get("href"):
+                                                Links = link.get("href")    # adding URI to source IDs of links to get URL
+                                        # print(Links)
+                                                d.append(Links)
+                                        else:
+                                                Links = url_news + link.get("href")
+                                        # print(Links)
+                                                d.append(Links)
+                        except Exception:
+                                links = None
+
+                d.pop(-1)  # removing last item for list which is not required
+                json_dict = defaultdict(list)
+                try:
+                        for item in d:
+                                title = item.split('/')[4]
+                                try:
+                                        json_dict[title].append(item)
                                 #print(json_dict)
-                        except Exception as e:
-                                print(e)
+                                except Exception as e:
+                                        print(e)
 
-        except Exception as e:
-                print(e)
+                except Exception as e:
+                        print(e)
 
-        return jsonify(json_dict)       # to decode JSON later in Flutter, converting obtained data to JSON
+                return jsonify(json_dict)       # to decode JSON later in Flutter, converting obtained data to JSON
 
 
 if __name__ == '__main__':
