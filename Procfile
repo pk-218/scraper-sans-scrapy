b0ea1ec80c9f464b9e82cc59d24556c85a4732a5
PankajK <pankajk218@users.noreply.github.com>
2020-06-21 00:28:37 +0530

    Removed jobs.py

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn run:app
-clock: python jobs.py
+clock: python collegescrapper.py
--- collegescrapper.py
+++ collegescrapper.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
+
 app = Flask(__name__)
 scheduler = APScheduler()
 
@@ -18,16 +19,16 @@ def linkScrapper():
         url_news = "https://www.vjti.ac.in"         # link to scrape links from
 
         # headers for complying with GET request
-        headers = {
-                'Accept-Encoding': 'gzip, deflate, sdch',
-                'Accept-Language': 'en-US,en;q=0.8',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Referer': 'http://www.wikipedia.org/',
-                'Connection': 'keep-alive',
-        }
+#        headers = {
+#                'Accept-Encoding': 'gzip, deflate, sdch',
+#                'Accept-Language': 'en-US,en;q=0.8',
+#                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+#                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+#                'Referer': 'http://www.wikipedia.org/',
+#                'Connection': 'keep-alive',
+#        }
 
-        r = requests.get(url=url_news, headers=headers)
+        r = requests.get(url=url_news)  #, headers=headers
         soup = BeautifulSoup(r.text, "html.parser")
         customs = soup.find_all("div", {"class": "custom"})
         for custom in customs:
@@ -47,27 +48,12 @@ def linkScrapper():
 
         d.pop(-1)  # removing last item for list which is not required
         json_dict = defaultdict(list)
-        """title = []
-        try:
-                for item in d:
-                        key_name = item.split('/')[4]
-                        title.append(key_name)
-        except Exception as e:
-                print(e)
-
-        try:
-                for i in range(len(title)):
-                        key = title[i]
-                        key_value = d[i]
-                        json_dict.update(dict.fromkeys(key, key_value))
-        except Exception as e:
-                print(e)"""""
         try:
                 for item in d:
                         title = item.split('/')[4]
                         try:
                                 json_dict[title].append(item)
-                                print(json_dict)
+                                #print(json_dict)
                         except Exception as e:
                                 print(e)
 
@@ -77,11 +63,7 @@ def linkScrapper():
         return jsonify(json_dict)       # to decode JSON later in Flutter, converting obtained data to JSON
 
 
-def scheduledTask():
-        linkScrapper()
-
-
 if __name__ == '__main__':
-        scheduler.add_job(id='Scheduled Task', func=scheduledTask, trigger='interval', seconds=5)
+        scheduler.add_job(id='Scheduled Task', func=linkScrapper, trigger='interval', seconds=2)
         scheduler.start()
-        app.run(port=5000)
+        app.run()
