7d91a0acd20dc00b60856c5114689c52ea46f3bb
Pankaj Khushalani <53428217+pk-218@users.noreply.github.com>
2020-06-23 16:30:33 +0530

    Update collegescrapper.py

--- collegescrapper.py
+++ collegescrapper.py
@@ -52,4 +52,4 @@ if __name__ == '__main__':
         scheduler.add_job(id='Scheduled Task', func=linkScrapper, trigger='interval', minutes=1)
         scheduler.start()
         port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+        app.run(port=port)
